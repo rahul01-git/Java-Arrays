d61327f34f43efbc8ab8175e7753ff51d2031ad3
rahul01-git <rahulneupane0090@gmail.com>
2023-02-23 10:38:35 +0545

    multi dimensional array

--- Array.java
+++ Array.java
@@ -1,23 +1,44 @@
 import java.util.Random;
 
-public class Array{
-    public static void main(String[] args){
+public class Array {
+    public static void main(String[] args) {
         // int[] x; //this one won't work
         // int[] x = new int[3];
         // int[] y = {10,20,30};
         // System.out.println(y[0]);
 
-        int[] x = new int[10];
-        for(int i = 0;i<10;i++){
-            Random random = new Random();
-            // generate 10 random values upto 10 and store it to array 
-            int n = random.nextInt(10);
-            x[i] = n;
-        }
+        // int[] x = new int[10];
+        // for(int i = 0;i<x.length;i++){
+        // Random random = new Random();
+        // // generate 10 random values upto 10 and store it to array
+        // int n = random.nextInt(10);
+        // x[i] = n;
+        // }
+
+        // for (int y : x) {
+        // System.out.println(y);
+        // }
+
+        // 2D Array
+        int[][] x = { { 10, 20, 30 },
+                { 20, 30, 40 },
+                { 50, 60, 70 } };
         
-        for (int y : x) {
-            System.out.println(y);
+        // // System.out.println(x[1][1]);
+
+        //NOTE: using for 
+        for(int i = 0; i< x.length;i++){
+            for(int j = 0;j<x.length;j++){
+                System.out.println(x[i][j]);
+            }
         }
-        
+
+        //NOTE: using foreach
+        // for (int[] y  : x) {
+        //     for (int z : y) {
+        //         System.out.println(z);
+        //     }
+        // }
     }
 }
+
